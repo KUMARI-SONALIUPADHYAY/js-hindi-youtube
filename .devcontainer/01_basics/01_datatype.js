40c26e0c294ed1d20669ada11ab942753fc5892d
KUMARI-SONALIUPADHYAY <upadhyaysonali2005@gmail.com>
2025-06-20 17:31:17 +0000

    configure datatype and varaible

---
+++ .devcontainer/01_basics/01_datatype.js
@@ -0,0 +1,16 @@
+console.log("sonali upadhyay")
+let name="sonali upadhyay"
+let age =28
+let isloggedin=false
+console.log(age)
+console.log(name)
+console.log(isloggedin);
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof isloggedin)
+console.log(typeof null)
+console.log(typeof undefined)
+
+
+
+
---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 12345
+let accountEmail = "sonaliupadhyay67@google.com"
+var accountPassword = "1276"
+accountCity = "jaipur"
+// accountId = 2
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity])
