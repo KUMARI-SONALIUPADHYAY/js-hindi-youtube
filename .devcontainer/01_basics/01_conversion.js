cd0aa12d95d0153f92134f41d1eff25cfe603a78
KUMARI-SONALIUPADHYAY <upadhyaysonali2005@gmail.com>
2025-06-25 17:03:25 +0000

    summary

---
+++ .devcontainer/01_basics/01_conversion.js
@@ -0,0 +1,52 @@
+// declaring a variable
+// let score = undefined
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let isloggedin = ""
+// let booleanisloggedin = Boolean(isloggedin)
+// console.log(booleanisloggedin)
+
+// let somenumber= null
+// let stringnumber = String(somenumber)
+// console.log(stringnumber);
+// console.log(typeof stringnumber);
+
+//** operation **//
+let value = 3
+let negvalue = -(value)
+console.log(negvalue)
+console.log(2+2)
+console.log(2**3);
+
+let str1 = "hello"
+let str2 = " hitish"
+str3 = str1 + str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+console.log(+true);
+console.log(+"");
+
+let gamecounter = 160;
+--gamecounter;
+console.log(gamecounter);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
