fb388768a31c664a134a1b836836012cd1a728c6
KUMARI-SONALIUPADHYAY <upadhyaysonali2005@gmail.com>
2025-07-02 15:37:03 +0000

    array update

---
+++ .devcontainer/01_basics/01_dateandtime.js
@@ -0,0 +1,33 @@
+// let mydate = new Date()
+// console.log(mydate)
+// console.log(mydate.toString());
+// console.log(mydate.toLocaleString());
+// console.log(typeof mydate);
+
+
+
+// let mycreatedDate = new Date(2023,0,23)
+// console.log(mycreatedDate);
+// console.log(mycreatedDate.toDateString());
+// console.log(mycreatedDate.toJSON());
+// console.log(mycreatedDate.toLocaleTimeString());
+// console.log(mycreatedDate.toTimeString());
+
+let mycreatedDate= new Date("2023-01-14")
+// console.log(mycreatedDate.toLocaleString());
+
+let mytimestamp = Date.now()
+// console.log(mytimestamp);
+// console.log(mycreatedDate.getMonth()+1);
+
+// console.log(Date.now());
+// console.log(Math.floor(Date.now()/1000));
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
+console.log(newDate.getFullYear())
+
+console.log(newDate.toLocaleString('default',{weekday:'long'}));
+
+
---
+++ .devcontainer/level_two/01_array.js
@@ -0,0 +1,31 @@
+const myarr=[0,1,2,3,4,5]
+// const myarr2 =new Array(1,2,3,4,5)
+// console.log(myarr[0]);
+// console.log(myarr.length);
+//push is used to add any element at last position
+// myarr.push(6)
+// myarr.push(7)
+// console.log(myarr);
+//pop - remove first element from array
+// myarr.pop()
+// console.log(myarr);
+//shift - used to remove element from 1st person
+//unshift- used to add at first position
+// myarr.shift()
+// console.log(myarr);
+// myarr.unshift(4)
+// console.log(myarr);
+// const myarr2 =new Array(9,8,7,6)
+// // console.log(myarr.includes(4));
+// // console.log(myarr.indexOf(3));
+// const newArray=myarr.join(myarr2)
+// console.log(newArray);
+// console.log(typeof newArray );
+console.log("A",myarr);
+const myn1=myarr.slice(1,3)
+console.log(myn1);
+
+console.log("B",myarr)
+const myn2=myarr.splice(1,3)
+console.log(myn2);
+
