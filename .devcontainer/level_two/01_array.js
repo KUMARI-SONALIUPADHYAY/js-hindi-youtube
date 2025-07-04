842b0f84678ead399632deb9812696bf495a27eb
KUMARI-SONALIUPADHYAY <upadhyaysonali2005@gmail.com>
2025-07-04 01:11:58 +0000

    array function

--- .devcontainer/level_two/01_array.js
+++ .devcontainer/level_two/01_array.js
@@ -1,4 +1,4 @@
-const myarr=[0,1,2,3,4,5]
+// const myarr=[0,1,2,3,4,5]
 // const myarr2 =new Array(1,2,3,4,5)
 // console.log(myarr[0]);
 // console.log(myarr.length);
@@ -21,11 +21,36 @@ const myarr=[0,1,2,3,4,5]
 // const newArray=myarr.join(myarr2)
 // console.log(newArray);
 // console.log(typeof newArray );
-console.log("A",myarr);
-const myn1=myarr.slice(1,3)
-console.log(myn1);
+// console.log("A",myarr);
+// const myn1=myarr.slice(1,3)
+// console.log(myn1);
 
-console.log("B",myarr)
-const myn2=myarr.splice(1,3)
-console.log(myn2);
+// console.log("B",myarr)
+// const myn2=myarr.splice(1,3)
+// console.log(myn2);
+const marvel_heros =['thor','ironman','spiderman']
+const dc=['superman','flash','batman']
+// marvel_heros.push(dc)
+// console.log(marvel_heros[2][1]);
+// marvel_heros.concat(dc)
+// console.log(marvel_heros);
+// const all_heros= marvel_heros.concat(dc)
+// console.log(all_heros);
+// const allnew_heros =[...marvel_heros,...dc]
+// console.log(allnew_heros);
+// const another_arr =[1,2,3,[4,5,6],7,[6,7,[4,5]]]
+// console.log(another_arr);
+// const real_an_array = another_arr.flat(Infinity)
+// console.log(real_an_array);
+// console.log(Array.isArray('hitish'));
+// console.log(Array.isArray([1,2,3]));
+// console.log(Array.from("hitish"));
+// console.log(Array.from({name:"hitish"}))
+// let score1 =100;
+// let score2 =200;
+// let score3 =300;
+// console.log(Array.of(score1,score2,score3));
+
+
+
 
